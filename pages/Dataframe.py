fc430823887a801bc2349dbaf37e9eb5dd977d0a
s26697 <s26697@pjwstk.edu.pl>
2025-11-06 11:35:28 +0100

    updated the multipage app template with documentation tutorial

--- pages/Dataframe.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def getData(path):
-    df = pd.DataFrame
-    pd.read_csv(path)
-    return df.set_index(df.columns[29])
-
-st.write("hello df")
---
+++ pages/page_3.py
@@ -0,0 +1,4 @@
+﻿import streamlit as st
+
+st.markdown("# Page 3 🎉")
+st.sidebar.markdown("# Page 3 🎉")
